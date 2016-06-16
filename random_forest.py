e7c636ae93a5a6b0706227e87e6c48ff5ad88da8
Ju Liu <ju.liu@welaika.com>
2016-06-16 11:15:33 +0200

    Add cross validation to random forest

--- random_forest.py
+++ random_forest.py
@@ -1,7 +1,7 @@
 import utils
 import numpy as np
 import pandas as pd
-from sklearn import ensemble
+from sklearn import ensemble, cross_validation
 
 train = pd.read_csv("./data/train.csv")
 test = pd.read_csv("./data/test.csv")
@@ -21,8 +21,8 @@ print "\nUse Random Forest classifier"
 
 forest = ensemble.RandomForestClassifier(
     max_depth = 7,
-    min_samples_split = 2,
-    n_estimators = 10000,
+    min_samples_split = 4,
+    n_estimators = 1000,
     random_state = 1,
     n_jobs = -1
 )
@@ -30,6 +30,10 @@ forest = forest.fit(features_forest, target)
 
 print(forest.feature_importances_)
 print(forest.score(features_forest, target))
+
+scores = cross_validation.cross_val_score(forest, features_forest, target, scoring='accuracy', cv=10)
+print scores
+print scores.mean()
 
 test_features_forest = test[["Pclass", "Age", "Sex", "Fare", "SibSp", "Parch", "Embarked"]].values
 prediction_forest = forest.predict(test_features_forest)
